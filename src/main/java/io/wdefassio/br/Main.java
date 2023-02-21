10439cd0d3bc2f6791a392418792c427d0ec95f4
Wellington Defassio <89281805+WellingtonDefassio@users.noreply.github.com>
2023-02-21 15:45:49 -0300

    commit all files

--- src/main/java/io/wdefassio/br/Main.java
+++ src/main/java/io/wdefassio/br/Main.java
@@ -2,6 +2,6 @@ package io.wdefassio.br;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
 }
